5a119dda5e679fe8b8f6f12545ff24c89ac3d2ca
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-02-15 13:49:34 -0500

    added floor

--- simulate.py
+++ simulate.py
@@ -1,10 +1,17 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# add gravity
 p.setGravity(0,0,-9.8)
 
+# add environment and objects
+planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
+
 for i in range(1000000):
     p.stepSimulation()
     time.sleep(.001)
