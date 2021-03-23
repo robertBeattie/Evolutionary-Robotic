7389b547a3395f0a3c689ec00d554550a7a86e43
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-03-22 20:20:55 -0400

    18

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -33,7 +33,7 @@ class NEURAL_NETWORK:
             if self.neurons[neuronName].Is_Sensor_Neuron():
                 self.neurons[neuronName].Update_Sensor_Neuron()
             else:
-                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron()
+                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron(self.neurons, self.synapses)
 
     def Get_Neuron_Names(self):
         return self.neurons
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -69,7 +69,7 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
-    def Update_Hidden_Or_Motor_Neuron(self):
+    def Update_Hidden_Or_Motor_Neuron(self, neurons, synapses):
         self.Set_Value(0.0)
 
 # -------------------------- Private methods -------------------------
