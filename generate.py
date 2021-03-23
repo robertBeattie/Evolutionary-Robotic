a0c2df9195dec63fe27e1ddeb2112554cccc59e9
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-03-22 21:11:09 -0400

    neuron

--- generate.py
+++ generate.py
@@ -45,6 +45,8 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
     pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=3, weight=1.0)
     pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1.0)
+    pyrosim.Send_Synapse(sourceNeuronName=0, targetNeuronName=4, weight=1.0)
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=1.0)
     pyrosim.End()
 
 
