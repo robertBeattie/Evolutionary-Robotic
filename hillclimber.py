e8cb7eb34c39528d99a6163f504bbd71de2c0871
Robert Beattie <50488761+robertBeattie@users.noreply.github.com>
2021-03-29 23:19:53 -0400

    70

--- hillclimber.py
+++ hillclimber.py
@@ -16,6 +16,7 @@ class HILL_CLIMBER:
         self.Spawn()
         self.Mutate()
         self.child.Evaluate()
+        self.Print()
         self.Select()
 
     def Spawn(self):
@@ -27,3 +28,6 @@ class HILL_CLIMBER:
     def Select(self):
         if self.parent.fitness < self.child.fitness:
             self.parent = self.child
+
+    def Print(self):
+        print('Parent:', self.parent.fitness, '<', 'Child:', self.child.fitness)
